4b35ce6a4b49cc56c1d1a002090fd983456a26f2
Shakil Ahmed <andomrohol4@gmail.com>
2021-12-04 02:36:40 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StepFrom from './Page/StepFrom';
+// import PrivateRoute from './Page/Login/PrivateRoute/PrivateRoute';
+
+function App() {
+  return (
+    <div className="App">
+      <StepFrom></StepFrom>
+    </div>
+  );
+}
+
+export default App;
